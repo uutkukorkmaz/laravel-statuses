c66fc46617282c41d737ad06458de2c5b82a5e16
uutkukorkmaz <uutkukorkmaz@gmail.com>
2022-11-27 18:04:28 +0300

    Add scopes for `HasStatus` concern

--- src/Concerns/HasStatus.php
+++ src/Concerns/HasStatus.php
@@ -2,14 +2,40 @@
 
 namespace Uutkukorkmaz\LaravelStatuses\Concerns;
 
+use Illuminate\Database\Eloquent\Builder;
+
 trait HasStatus
 {
-
     public function initializeHasStatus(): void
     {
-        if(!in_array('status', $this->fillable)){
+        if (! in_array('status', $this->fillable)) {
             $this->fillable[] = 'status';
         }
     }
 
+    public function scopeStatus(Builder $builder, string $status): Builder
+    {
+        return $builder->where('status', $status);
+    }
+
+    public function scopeStatusNot(Builder $builder, string $status): Builder
+    {
+        return $builder->where('status', '!=', $status);
+    }
+
+    public function statusNext(): void
+    {
+        if (method_exists(get_class($this->status), 'next')) {
+            $this->status = $this->status->next();
+            $this->save();
+        }
+    }
+
+    public function statusPrevious(): void
+    {
+        if (method_exists(get_class($this->status), 'previous')) {
+            $this->status = $this->status->previous();
+            $this->save();
+        }
+    }
 }
