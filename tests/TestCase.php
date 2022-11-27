2b2ddff691e9f7f82e6bca1637319b35b5a110ce
uutkukorkmaz <uutkukorkmaz@gmail.com>
2022-11-27 18:04:54 +0300

    Clean up test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,7 @@
 
 namespace Uutkukorkmaz\LaravelStatuses\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\File;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Uutkukorkmaz\LaravelStatuses\LaravelStatusesServiceProvider;
 
@@ -12,9 +12,10 @@ class TestCase extends Orchestra
     {
         parent::setUp();
 
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Uutkukorkmaz\\LaravelStatuses\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
+        $this->beforeApplicationDestroyed(function () {
+            File::cleanDirectory(app_path('Enums'));
+            File::cleanDirectory(app_path('Models'));
+        });
     }
 
     protected function getPackageProviders($app)
@@ -27,10 +28,5 @@ class TestCase extends Orchestra
     public function getEnvironmentSetUp($app)
     {
         config()->set('database.default', 'testing');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_laravel-statuses_table.php.stub';
-        $migration->up();
-        */
     }
 }
